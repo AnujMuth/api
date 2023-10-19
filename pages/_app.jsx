111840c99e0e1d6b6648ce1c63a69740e6d01185
AnujMuth <kelvin1314a@gmail.com>
2023-10-19 20:44:07 +0800

    first commit

---
+++ pages/_app.jsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
+import { ThemeProvider } from 'next-themes'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class">
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </ThemeProvider>
+  )
+}
